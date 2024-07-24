7d4ca7be767768f6fc8c8b776980cf50ca5627c0
Hein Htet <littleshadow.mdy@gmail.com>
2024-07-24 16:18:59 +0900

    ROT13

---
+++ ROT13.js
@@ -0,0 +1,21 @@
+function rot13(str) {
+  input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  temp = "";
+  answer = "";
+  result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (!input.includes(str[i])) {
+      temp += str[i];
+    } else {
+      for (let j = 0; j < input.length; j++) {
+        if (str[i] === input[j]) {
+          temp += output[j];
+        }
+      }
+    }
+  }
+  console.log(temp);
+}
+
+rot13("This is my first ROT13 excercise!");
